c49aa960db4cb888f95f5709d74668718deeadb7
Dmitriy1604 <Dimon312-91@mail.ru>
2022-07-07 21:47:39 +0300

    homework1

---
+++ src/main/java/com/example/demo/speed/swe.java
@@ -0,0 +1,23 @@
+package com.example.demo.speed;
+
+public class swe {
+    public static void main(String[] args) {
+        System.out.println("Люк я твой отец");
+        System.out.println("неееееет");
+        System.out.println("познай силу");
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
